c4674ab01cea5332936ba08e54b5e1664c13b13c
BiniTutor <binitutor1@gmail.com>
2024-12-25 10:40:13 -0500

    starter

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const inputName = document.getElementById('name')
+const inputEmail = document.getElementById('email')
+const outputName = document.getElementById('outputName')
+const outputEmail = document.getElementById('outputEmail')
+
+function save_data(event){
+    event.preventDefault()
+    alert('form submitted!')    
+}
